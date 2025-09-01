7133ad158c0d932dac3721c502c49d01b4ad7090
Kalum Gunawardhana <kguna415@gmail.com>
2025-09-01 11:52:14 +0530

    commit

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import notesRoutes from './routes/notesRoutes.js';
+import { connectDB } from './config/db.js';
 
 const app = express();
 
+const PORT = process.env.PORT || 5001;
+
+connectDB();
+
 app.use("/api/notes", notesRoutes);
 
-app.listen(5001, () => {
-    console.log("Server is running on port: 5001");
+app.listen(PORT, () => {
+    console.log("Server is running on port: ", PORT);
 });
