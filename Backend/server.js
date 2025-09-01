ed6c5a5726c0b4d187889117b31acfa7ef9ce730
Kalum Gunawardhana <kguna415@gmail.com>
2025-09-01 09:58:39 +0530

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -8,5 +8,20 @@ app.listen(5001, ()=> {
 
 //send the notes to the frontend
 app.get("/api/notes", (req, res)=>{
-    res.send("you got 5 notes");
+    res.status(200).send("You got 5 notes");
+});
+
+//create a new note
+app.post("/api/notes", (req, res)=>{
+    res.status(201).json({message: "Note created successfully" });
+});
+
+//update a note
+app.put("/api/notes/:id", (req, res)=>{
+    res.status(200).json({message: "Note updated successfully" });
+});
+
+//delete a note
+app.delete("/api/notes/:id", (req, res)=>{
+    res.status(200).json({message: "Note deleted successfully" });
 });
