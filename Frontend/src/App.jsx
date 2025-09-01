1905595c2e135950bca047feb2cf10ee1d3b8603
Kalum Gunawardhana <kguna415@gmail.com>
2025-09-01 20:59:04 +0530

    commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,7 +8,6 @@ import { toast } from 'react-hot-toast'
 const App = () => {
   return (
     <div>
-      <button onClick={() => toast.success("Congrats!")}>Toggle Theme</button>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
