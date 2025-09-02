30738afae9ca75df71a84c58bdc80c0be860f841
Kalum Gunawardhana <kguna415@gmail.com>
2025-09-02 12:12:53 +0530

    commit

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -43,7 +43,7 @@ const HomePage = () => {
             <div className='max-w-7xl mx-auto p-4 mt-6'>
                 {loading && <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>Loading notes...</div>}
                 {notes.map((note) => (
-                    <NoteCard key={note._id} note={note} />
+                    <NoteCard key={note._id} note={note} setNotes={setNotes} />
                 ))}
             </div>
         </div>
