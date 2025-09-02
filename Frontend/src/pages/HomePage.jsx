1807608ffb2d2d442d33ed63dd595016d970f188
Kalum Gunawardhana <kguna415@gmail.com>
2025-09-02 12:26:31 +0530

    commit

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -6,6 +6,7 @@ import axios from 'axios';
 import toast from 'react-hot-toast';
 import NoteCard from '../components/NoteCard';
 import api from '../lib/axios';
+import NotesNotFound from '../components/NotesNotFound';
 
 const HomePage = () => {
     const [isRateLimited, setIsRateLimited] = useState(true);
@@ -42,6 +43,9 @@ const HomePage = () => {
 
             <div className='max-w-7xl mx-auto p-4 mt-6'>
                 {loading && <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>Loading notes...</div>}
+
+                {notes.length===0 && !isRateLimited && <NotesNotFound />}
+
                 {notes.map((note) => (
                     <NoteCard key={note._id} note={note} setNotes={setNotes} />
                 ))}
