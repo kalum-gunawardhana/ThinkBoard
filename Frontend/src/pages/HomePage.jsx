dbe7ca5bb8883e6d606a43d03d5bc90586b17953
Kalum Gunawardhana <kguna415@gmail.com>
2025-09-02 08:04:16 +0530

    commit

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -4,6 +4,22 @@ import RateLimitedUI from '../components/RateLimitedUI';
 
 const HomePage = () => {
     const [isRateLimited, setIsRateLimited] = useState(true);
+    const [notes, setNotes] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const fetchNotes = async () => {
+            try {
+                const res = await fetch("http://localhost:8080/api/notes");
+                const data = await res.json();
+                console.log(data);
+            } catch (error) {
+                console.log("Error fetching notes:", error);
+            }
+        }
+
+        fetchNotes();
+    }, []);
 
     return (
         <div className='min-h-screen'>
