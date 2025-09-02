d9f6b8d112a275a52b307f830664173a045af207
Kalum Gunawardhana <kguna415@gmail.com>
2025-09-02 11:28:57 +0530

    commit

--- Frontend/src/pages/CreatePage.jsx
+++ Frontend/src/pages/CreatePage.jsx
@@ -1,14 +1,42 @@
+import axios from 'axios';
 import { ArrowLeftIcon } from 'lucide-react';
 import React from 'react'
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import toast from 'react-hot-toast';
+import { Link, useNavigate } from 'react-router-dom';
 
 const CreatePage = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [loading, setLoading] = useState(false);
 
-  const handleSubmit = () => { };
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // console.log(title);
+    // console.log(content);
+
+    if (!title.trim() || !content.trim()) {
+      toast.error("Please fill in all fields");
+      return;
+    }
+
+    setLoading(true);
+    try {
+      await axios.post("http://localhost:5001/api/notes", {
+        title,
+        content
+      }),
+        toast.success("Note created successfully");
+      navigate("/");
+    } catch (error) {
+      console.log("Error creating note:", error);
+      toast.error("Failed to create note");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <div className='min-h-screen bg-base-200'>
@@ -49,7 +77,11 @@ const CreatePage = () => {
                   />
                 </div>
 
-                
+                <div className='card-actions justify-end'>
+                  <button type='submit' className='btn btn-primary' disabled={loading}>
+                    {loading ? "Creating..." : "Create Note"}
+                  </button>
+                </div>
               </form>
             </div>
           </div>
