84b1872d3b57c1fad95bbecc785235cdfed9ae7b
Kalum Gunawardhana <kguna415@gmail.com>
2025-09-02 11:04:24 +0530

    commit

--- Frontend/src/pages/CreatePage.jsx
+++ Frontend/src/pages/CreatePage.jsx
@@ -1,9 +1,45 @@
+import { ArrowLeftIcon } from 'lucide-react';
 import React from 'react'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const CreatePage = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = () => { };
+
   return (
-    <div>
-      <h1>Create Page</h1>
+    <div className='min-h-screen bg-base-200'>
+      <div className='container mx-auto p-4'>
+        <div className='max-w-2xl mx-auto'>
+          <Link to={"/"} className='btn btn-ghost mb-6'>
+            <ArrowLeftIcon className='size-5' />
+            Back to Notes
+          </Link>
+
+          <div className='card bg-base-100'>
+            <div className='card-body'>
+              <h2 className='card-title text-2xl mb-4'>Create New Note</h2>
+              <form onSubmit={handleSubmit}>
+                <div className='form-control mb-4'>
+                  <label className='label'>
+                    <span className='label-text'>Title</span>
+                  </label>
+                  <input
+                    type='text'
+                    placeholder='Note Title'
+                    className='input input-bordered'
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                  />
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
