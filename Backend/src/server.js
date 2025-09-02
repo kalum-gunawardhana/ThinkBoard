9ecb9a8243b84b4f5f3a44e80396c541b8c110a7
Kalum Gunawardhana <kguna415@gmail.com>
2025-09-02 09:47:12 +0530

    commit

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import notesRoutes from './routes/notesRoutes.js';
 import { connectDB } from './config/db.js';
 import rateLimiter from './middleware/rateLimiter.js';
+import cros from 'cors';
 
 const app = express();
 
@@ -10,6 +11,10 @@ const PORT = process.env.PORT || 5001;
 connectDB();
 
 //middleware to parse JSON request bodies
+app.use(
+    cros({
+        origin: "http://localhost:5173"
+    }));
 app.use(express.json());
 app.use(rateLimiter);
 
--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,20 +1,31 @@
 import React, { useState } from 'react'
 import Navbar from '../components/Navbar'
 import RateLimitedUI from '../components/RateLimitedUI';
+import { useEffect } from 'react';
+import axios from 'axios';
+import toast from 'react-hot-toast';
 
 const HomePage = () => {
     const [isRateLimited, setIsRateLimited] = useState(true);
     const [notes, setNotes] = useState([]);
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchNotes = async () => {
             try {
-                const res = await fetch("http://localhost:8080/api/notes");
-                const data = await res.json();
-                console.log(data);
+                const res = await axios.get("http://localhost:5001/api/notes");
+                console.log(res.data);
+                setNotes(res.data);
+                setIsRateLimited(false);
             } catch (error) {
                 console.log("Error fetching notes:", error);
+                if (error.response && error.response.status === 429) {
+                    setIsRateLimited(true);
+                } else {
+                    toast.error("Error fetching notes");
+                }
+            } finally {
+                setLoading(false);
             }
         }
 
@@ -26,6 +37,13 @@ const HomePage = () => {
             <Navbar />
 
             {isRateLimited && <RateLimitedUI />}
+
+            <div className='max-w-7xl mx-auto p-4 mt-6'>
+                {loading && <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>Loading notes...</div>}
+                {notes.map((note) => (
+                    <NoteCard key={note._id} note={note} />
+                ))}
+            </div>
         </div>
     )
 }
